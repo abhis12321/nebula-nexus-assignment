69a38b5cf9cafe004d8107aab392fccf9a25d887
abhis12321 <abhisheksingh2359328@gmail.com>
2024-06-22 15:09:54 +0530

    Blogs are being rendered using GET API

--- app/page.js
+++ app/page.js
@@ -1,12 +1,30 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BlogForm from './_components/BlogForm'
+import axios from 'axios';
+import BlogCard from './_components/BlogCard';
 
 export default function page() {
+  const [blogs , setBlogs] = useState([]);
+
+  useEffect(() => {
+    axios.get('/blogAPI')
+        .then(response => response.data)
+        .then(data => data.success ? setBlogs(data.blogs) : console.log(data.message))
+        .catch(error => console.log(error.message));
+  });
+
+
   return (
     <div>
       statring the assignment
       <BlogForm />
+
+      <div className="flex gap-4 p-4 flex-col items-center justify-center">
+        {
+          blogs?.map((blog , index) => <BlogCard key={blog._id} blog={blog} />)
+        }
+      </div>
     </div>
   )
 }
