3ea0ee204ee43e8db37117ea3402c67cdf694bd9
abhis12321 <abhisheksingh2359328@gmail.com>
2024-06-22 12:50:28 +0530

    BlogCard is created to show specific blogs

---
+++ app/_components/BlogCard.js
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function BlogCard({ blog }) {
+    return (
+        <div className=''>
+            <Image src={blog.imgURL} className=''/>
+            <h2 className="">{blog.title}</h2>
+            <p className="">{blog.content}</p>
+            <Link href={blog.lnkdnURL} className=''>{blog.authorName}</Link>
+        </div>
+    )
+}
