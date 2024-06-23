4161e931547163e4ca55bacfb40f026584181777
abhis12321 <abhisheksingh2359328@gmail.com>
2024-06-23 21:06:51 +0530

    Title is added for dynamic blogs with the help of Next-Head

--- app/[blog]/page.js
+++ app/[blog]/page.js
@@ -2,6 +2,7 @@
 import axios from 'axios';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
+import Head from 'next/head';
 
 export default function page({ params }) {
     const [blog, setBlog] = useState();
@@ -19,19 +20,22 @@ export default function page({ params }) {
 
 
     return (
-        <div className='flex items-center justify-center flex-col px-1 sm:px-2 py-4'>
-            {
-                blog ?
-                    <div className='ring-2 bg-blue-900/10 px-2 sm:px-3 pt-3 pb-5 rounded w-[98%] max-w-[750px] flex flex-col items-start justify-start relative'>
-                        <div className="absolute bottom-2 left-3 px-2 py-1 rounded cursor-pointer font-semibold text-red-950 hover:bg-gray-700/20 text-sm bg-gray-700/10 shadow-[0_0_2px_gray]" onClick={copyURL}>share</div>
-                        <img src={blog?.imgURL} alt='blog-image' className='rounded self-center w-full' width={400} height={350} />
-                        <h2 className="sm:px-1 py-2 text-2xl font-bold font-serif text-red-950">{blog?.title}</h2>
-                        <div className="sm:px-1 font-mono text-gray-600 text-sm">{blog?.content}</div>
-                        <Link href={`${blog ? blog.linkdInProfileURL : '/'}`} target='_blank' className='px-2 text-[11px] hover:underline self-end font-semibold text-blue-800'>__{blog?.authorName.toLowerCase()}</Link>
-                    </div>
-                    :
-                    <div className="">Invalid request</div>
-            }
-        </div>
+        <Head>
+            <title>{blog ? blog.title : params.blog}</title>
+            <div className='flex items-center justify-center flex-col px-1 sm:px-2 py-4'>
+                {
+                    blog ?
+                        <div className='ring-2 bg-blue-900/10 px-2 sm:px-3 pt-3 pb-5 rounded w-[98%] max-w-[750px] flex flex-col items-start justify-start relative'>
+                            <div className="absolute bottom-2 left-3 px-2 py-1 rounded cursor-pointer font-semibold text-red-950 hover:bg-gray-700/20 text-sm bg-gray-700/10 shadow-[0_0_2px_gray]" onClick={copyURL}>share</div>
+                            <img src={blog?.imgURL} alt='blog-image' className='rounded self-center w-full' width={400} height={350} />
+                            <h2 className="sm:px-1 py-2 text-2xl font-bold font-serif text-red-950">{blog?.title}</h2>
+                            <div className="sm:px-1 font-mono text-gray-600 text-sm">{blog?.content}</div>
+                            <Link href={`${blog ? blog.linkdInProfileURL : '/'}`} target='_blank' className='px-2 text-[11px] hover:underline self-end font-semibold text-blue-800'>__{blog?.authorName.toLowerCase()}</Link>
+                        </div>
+                        :
+                        <div className="">Invalid request</div>
+                }
+            </div>
+        </Head>
     )
 }
